159c9058fb7c1c10063ecc60c38ae7911576b1ce
DKnight <DKnight1010@gmail.com>
2015-08-20 10:11:34 -0400

    Fixed fetching last date from DB to default to opening day when there are no records

--- lib/query_db.py
+++ lib/query_db.py
@@ -1,41 +1,42 @@
 #import db_connect
 from DB import DB
+import datetime
 #import write_missing
 
-def check_id(name, player_id, sport):
-    """
-    Checks if the player id exists in the player_ids table and tried to
-    update it if not.
-    :param name:
-        name in the format 'first last'
-    :param player_id:
-        roto guru id number.
-    :return:
-    """
-
-    conn, cur = db_connect.connect(sport)
-
-    if sport == 'baseball':
-        cur.execute("SELECT COUNT(*) FROM player_ids WHERE rguru_id = %s", (player_id))
-        num_rguru_id = cur.fetchone()[0]
-        if num_rguru_id == 1:
-            return
-        else:
-            fname, lname = name.split(' ', 1)
-            cur.execute("SELECT COUNT(*), bbm_id FROM player_ids WHERE first_name=%s AND last_name=%s",
-                        (fname, lname))
-            cursor_val = cur.fetchone()
-            name_counts = cursor_val[0]
-            if name_counts == 1:
-                bbm_id = cursor_val[1]
-                cur.execute("UPDATE player_ids SET rguru_id=%s WHERE bbm_id=%s",
-                            (player_id, bbm_id))
-                conn.commit()
-            elif name_counts > 1:
-                write_missing.missing_player(name, player_id, sport, True)
-
-            elif name_counts == 0:
-                write_missing.missing_player(name, player_id, sport)
+# def check_id(name, player_id, sport):
+#     """
+#     Checks if the player id exists in the player_ids table and tried to
+#     update it if not.
+#     :param name:
+#         name in the format 'first last'
+#     :param player_id:
+#         roto guru id number.
+#     :return:
+#     """
+#
+#     conn, cur = db_connect.connect(sport)
+#
+#     if sport == 'baseball':
+#         cur.execute("SELECT COUNT(*) FROM player_ids WHERE rguru_id = %s", (player_id))
+#         num_rguru_id = cur.fetchone()[0]
+#         if num_rguru_id == 1:
+#             return
+#         else:
+#             fname, lname = name.split(' ', 1)
+#             cur.execute("SELECT COUNT(*), bbm_id FROM player_ids WHERE first_name=%s AND last_name=%s",
+#                         (fname, lname))
+#             cursor_val = cur.fetchone()
+#             name_counts = cursor_val[0]
+#             if name_counts == 1:
+#                 bbm_id = cursor_val[1]
+#                 cur.execute("UPDATE player_ids SET rguru_id=%s WHERE bbm_id=%s",
+#                             (player_id, bbm_id))
+#                 conn.commit()
+#             elif name_counts > 1:
+#                 write_missing.missing_player(name, player_id, sport, True)
+#
+#             elif name_counts == 0:
+#                 write_missing.missing_player(name, player_id, sport)
 
 def get_last_entry(sport):
     """
@@ -45,6 +46,11 @@ def get_last_entry(sport):
     cursor = DB()
 
     last_date = cursor.query("SELECT MAX(date) AS date from rguru_hitters")[0][0]
+
+    # If no entries in DB, get first date of season.
+    if last_date is None:
+        if sport == 'baseball':
+            last_date = datetime.date(2015, 4, 5)
 
     cursor.finish()
 
